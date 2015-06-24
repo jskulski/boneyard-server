6bebd4b7319a08cf57c63cb11dc1afc9950dd0db
Jon Skulski <jskulski@gmail.com>
2015-06-23 18:42:05 -0700

    Return 'some' data. ({} is data, right?!)

--- Boneyard/FlaskApp.py
+++ Boneyard/FlaskApp.py
@@ -1,8 +1,15 @@
 from flask import Flask
 
+import json
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def homepage():
     return "lol"
+
+@app.route('/card-stack')
+def retrieve_card_stack():
+    return json.dumps({})
--- test/flask_acceptance_test.py
+++ test/flask_acceptance_test.py
@@ -1,5 +1,7 @@
 import pytest
 
+import json
+
 from Boneyard import FlaskApp
 
 def client():
@@ -8,8 +10,16 @@ def client():
 
 def test_can_get_home_page():
     response = client().get('/')
+    assert response.status_code == 200
 
-    print response.data
+def test_card_stack_is_empty_on_first_request():
+    response = client().get('/card-stack')
 
     assert response.status_code == 200
+    assert json.loads(response.data) == {}
+
+
+
+
+
 
