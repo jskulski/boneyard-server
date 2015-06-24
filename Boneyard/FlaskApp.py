ddcf4546b379a460cb9001eedcad290c4e9bd476
Jon Skulski <jskulski@gmail.com>
2015-06-23 18:47:42 -0700

    Cleaning up, removing homepage and changing url structure

--- Boneyard/FlaskApp.py
+++ Boneyard/FlaskApp.py
@@ -5,11 +5,10 @@ import json
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def homepage():
-    return "lol"
-
-@app.route('/card-stack')
+@app.route('/cards', methods=['GET'])
 def retrieve_card_stack():
     return json.dumps({})
+
+@app.route('/card', methods=['POST'])
+def create_new_card():
+    return 'hello!'
--- test/flask_acceptance_test.py
+++ test/flask_acceptance_test.py
@@ -8,15 +8,19 @@ def client():
     client = FlaskApp.app.test_client()
     return client
 
-def test_can_get_home_page():
-    response = client().get('/')
-    assert response.status_code == 200
-
 def test_card_stack_is_empty_on_first_request():
-    response = client().get('/card-stack')
+    response = client().get('/cards')
 
     assert response.status_code == 200
     assert json.loads(response.data) == {}
+
+def test_we_can_post_to_create_a_new_card():
+    response = client().post('/card', data=dict(
+        body="This is the body of the card"
+    ))
+
+    assert response.status_code == 200
+
 
 
 
